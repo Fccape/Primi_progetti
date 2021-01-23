11f2915627b8568b4558b9bb9543061230c75638
Fccape <76962609+Fccape@users.noreply.github.com>
2021-01-23 15:03:17 +0100

    Update gmap.ts

--- Angular/Demo/src/app/components/gmap.ts
+++ Angular/Demo/src/app/components/gmap.ts
@@ -2,7 +2,7 @@ import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'gmap',
-  template: ` <div>{{ nome }} ha {{ eta }} anni</div> `,
+  template: ` <div>{{ nome }} ha {{ eta }} anni!</div> `,
 })
 export class MapComponent {
   @Input() nome: string = '';
