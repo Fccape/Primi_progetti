026b4800fa06876c8b8e796d3a331b9309f20112
Fccape <76962609+Fccape@users.noreply.github.com>
2021-01-23 01:14:11 +0100

    Angular

---
+++ Angular/Demo/src/app/app.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  public counter = 0;
+
+  public url = '';
+  public heightValue = 50;
+
+  public renderImage(): void {
+    this.url =
+      'https://angular.io/assets/images/logos/angularjs/AngularJS-Shield.svg';
+  }
+  public nullifyImage(): void {
+    this.url = '';
+  }
+
+  sum(a: number, b: number): number {
+    return a + b;
+  }
+
+  public inc(): void {
+    this.counter += 1;
+  }
+}
