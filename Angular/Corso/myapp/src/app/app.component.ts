7f08fc8f6029b35a962e8fddaaf6d9012ef54568
Fccape <76962609+Fccape@users.noreply.github.com>
2021-02-08 21:47:01 +0100

    push

--- Angular/Corso/myapp/src/app/app.component.ts
+++ Angular/Corso/myapp/src/app/app.component.ts
@@ -1,33 +1,11 @@
 import { User, Company } from './model/user';
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { NgForm } from '@angular/forms';
 @Component({
   selector: 'app-root',
   // templateUrl: './app.component.html',
-  template: `
-    <div
-      *ngFor="
-        let user of users;
-        let i = index;
-        let dispari = odd;
-        let last = last
-      "
-    >
-      {{ i + 1 }}. {{ user.name }} {{ user.company.name }}
-      <div *ngIf="dispari"><hr /></div>
-      <div *ngIf="last">End of list</div>
-    </div>
-  `,
+  templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  users!: User[];
-
-  constructor(http: HttpClient) {
-    setTimeout(() => {
-      http
-        .get<User[]>('https://jsonplaceholder.typicode.com/users')
-        .subscribe((result) => (this.users = result));
-    }, 1000);
-  }
-}
+export class AppComponent {}
